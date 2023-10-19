7b8f9c0d6e93bd9cf94ad75f2dc0edb83564b13c
Stan van der Linde <stan.vanderlinde@tno.nl>
2023-10-19 15:02:19 +0200

    Move 'paretor front' computation to different file

--- scripts/Port_Opt_Rabo_v3.py
+++ scripts/Port_Opt_Rabo_v3.py
@@ -9,100 +9,7 @@ from dwave.system.samplers import DWaveSampler  # Library to interact with the Q
 from minorminer import find_embedding
 from pyqubo import Array, Constraint, Placeholder
 
-
-def pareto_front_v3(x3, y3, res_ctr3):
-    for j in range(res_ctr3):
-        for k in range(res_ctr3):
-            if (x3[j] > 0) | (y3[j] > 0):
-                if (x3[k] > 0) | (y3[k] > 0):
-                    if y3[k] < y3[j]:
-                        for i in range(res_ctr3):
-                            if (x3[i] > 0) | (y3[i] > 0):
-                                if (
-                                    (x3[i] < x3[j])
-                                    & (y3[i] < y3[j])
-                                    & (x3[i] < x3[k])
-                                    & (y3[i] > y3[k])
-                                ):
-                                    x3[i] = 0
-                                    y3[i] = 0
-                                elif (
-                                    (x3[j] < x3[i])
-                                    & (y3[j] < y3[i])
-                                    & (x3[j] < x3[k])
-                                    & (y3[j] > y3[k])
-                                ):
-                                    x3[j] = 0
-                                    y3[j] = 0
-                                    break
-                                elif (
-                                    (x3[k] < x3[j])
-                                    & (y3[k] < y3[j])
-                                    & (x3[k] < x3[i])
-                                    & (y3[k] > y3[i])
-                                ):
-                                    x3[k] = 0
-                                    y3[k] = 0
-                                    break
-                            else:
-                                x3[i] = 0
-                                y3[i] = 0
-            else:
-                break
-    return x3, y3
-
-
-def coef(j, n):
-    return 0.25 * (-(n // 2) + j)
-
-
-def pareto_front(x, y, ctr):
-    n = 41
-    rb = []
-    for i in range(n):
-        rb.append(0)
-    for i in range(ctr):
-        for j in range(n):
-            if x[i] + coef(j, n) * y[i] > x[rb[j]] + coef(j, n) * y[rb[j]]:
-                rb[j] = i
-
-    u = {}
-    v = {}
-    c = 0
-
-    l1 = []
-    for j in range(n - 1):
-        l1.append(0)
-    l2 = 0
-    for i in range(ctr):
-        if (x[i] > 0) | (y[i] > 0):
-            remain = True
-            for j in range(n - 1):
-                if (
-                    (x[i] < x[rb[j + 1]])
-                    & (y[i] < y[rb[j + 1]])
-                    & (x[i] < x[rb[j]])
-                    & (y[i] > y[rb[j]])
-                ):
-                    x[i] = 0
-                    y[i] = 0
-                    l1[j] += 1
-                    remain = False
-                    break
-            if remain == True:
-                u[c] = x[i]
-                v[c] = y[i]
-                c += 1
-        else:
-            x[i] = 0
-            y[i] = 0
-            l2 += 1
-
-    print(l1, l2, ctr, c)
-    x, y = pareto_front_v3(u, v, c)
-
-    return x, y
-
+from tno.quantum.problems.portfolio_optimization.pareto_front import pareto_front
 
 # Number of assets
 N = 52
@@ -505,9 +412,11 @@ print(name)
 # plt.show()
 
 starttime = datetime.datetime.now()
+
 x1, y1 = pareto_front(x1, y1, res_ctr1)
 x2, y2 = pareto_front(x2, y2, res_ctr2)
 x3, y3 = pareto_front(x3, y3, res_ctr3)
+
 print("Time consumed:", datetime.datetime.now() - starttime)
 
 # Make a plot of the results.
