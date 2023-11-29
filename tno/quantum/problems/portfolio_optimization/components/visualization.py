7f38d86feb7dad2e4d8b9ff515518589f2ed1cdc
Wezeman <robert.wezeman@tno.nl>
2023-11-29 19:36:05 +0100

    Add vmin, vmax

--- tno/quantum/problems/portfolio_optimization/components/visualization.py
+++ tno/quantum/problems/portfolio_optimization/components/visualization.py
@@ -11,6 +11,7 @@ from tno.quantum.problems.portfolio_optimization.components.postprocess import (
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
+    from typing import Any
 
     from matplotlib.axes import Axes
     from matplotlib.collections import PatchCollection
@@ -25,10 +26,12 @@ def plot_points(
     color: str | None = None,
     label: str | None = None,
     c: ArrayLike | Sequence[ColorType] | ColorType | None = None,
+    vmin: float | None = None,
+    vmax: float | None = None,
     alpha: float | None = None,
     cmap: str | Colormap | None = None,
     ax: Axes | None = None,  # pylint: disable=invalid-name
-) -> PatchCollection:
+) -> PatchCollection | Any:
     """Plots the given data-points in a Diversification-ROC plot.
 
     Args:
@@ -39,9 +42,11 @@ def plot_points(
             will be assigned by ``matplotlib``. Default is ``None``.
         label: Label to use in the legend. If ``None`` is given, no label will be used.
             Default is ``None``.
-        c: The marker colors as to be used by ``matplotlib``.
-        alpha: The alpha blending value as to be used by ``matplotlib``.
-        cmap: The Colormap instance or registered colormap name used by ``matplotlib``.
+        c: The marker colors as used by ``matplotlib``.
+        vmin: min value of data range that colormap covers as used by ``matplotlib``.
+        vmax: max value of data range that colormap covers as used by ``matplotlib``.
+        alpha: The alpha blending value as used by ``matplotlib``.
+        cmap: The Colormap instance or registered colormap name as used by ``matplotlib``.
         ax:  ``Axes`` to plot on. If ``None``, a new figure with one ``Axes`` will be
             created.
 
@@ -58,6 +63,8 @@ def plot_points(
         color=color,
         c=c,
         alpha=alpha,
+        vmin=vmin,
+        vmax=vmax,
         cmap=cmap,
         label=label,
     )
@@ -68,8 +75,8 @@ def plot_points(
 
     xlim = ax.get_xlim()
     ylim = ax.get_ylim()
-    ax.hlines(0, xlim[0], xlim[1], colors=["black"], lw=1)
-    ax.vlines(0, ylim[0], ylim[1], colors=["black"], lw=1)
+    ax.hlines(0, float("-inf"), float("inf"), colors=["black"], lw=1)
+    ax.vlines(0, float("-inf"), float("inf"), colors=["black"], lw=1)
     ax.set_xlim(*xlim, auto=True)
     ax.set_ylim(*ylim, auto=True)
     return collection
@@ -81,6 +88,8 @@ def plot_front(
     color: str | None = None,
     label: str | None = None,
     c: ArrayLike | Sequence[ColorType] | ColorType | None = None,
+    vmin: float | None = None,
+    vmax: float | None = None,
     alpha: float | None = None,
     cmap: str | Colormap | None = None,
     ax: Axes | None = None,  # pylint: disable=invalid-name
@@ -95,9 +104,11 @@ def plot_front(
             will be assigned by ``matplotlib``. Default is ``None``.
         label: Label to use in the legend. If ``None`` is given, no label will be used.
             Default is ``None``.
-        c: The marker colors as to be used by ``matplotlib``.
-        alpha: The alpha blending value as to be used by ``matplotlib``.
-        cmap: The Colormap instance or registered colormap name used by ``matplotlib``.
+        c: The marker colors as used by ``matplotlib``.
+        vmin: min value of data range that colormap covers as used by ``matplotlib``.
+        vmax: max value of data range that colormap covers as used by ``matplotlib``.
+        alpha: The alpha blending value as used by ``matplotlib``.
+        cmap: The Colormap instance or registered colormap name as used by ``matplotlib``.
         ax:  ``Axes`` to plot on. If ``None``, a new figure with one ``Axes`` will be
             created.
 
@@ -108,5 +119,14 @@ def plot_front(
     """
     x_values, y_values = pareto_front(diversification_values, roc_values)
     return plot_points(
-        x_values, y_values, color=color, c=c, alpha=alpha, cmap=cmap, label=label, ax=ax
+        x_values,
+        y_values,
+        color=color,
+        c=c,
+        alpha=alpha,
+        vmin=vmin,
+        vmax=vmax,
+        cmap=cmap,
+        label=label,
+        ax=ax,
     )
