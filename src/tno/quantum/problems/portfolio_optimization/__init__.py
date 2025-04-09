55abc33600154148591963a6e025dd35d3fef366
Wezeman <robert.wezeman@tno.nl>
2025-04-09 10:26:17 +0200

    Move image to assets

--- src/tno/quantum/problems/portfolio_optimization/__init__.py
+++ src/tno/quantum/problems/portfolio_optimization/__init__.py
@@ -72,7 +72,7 @@ worsening the other objective, can be computed for return on capital and diversi
 >>> fig.tight_layout()
 >>> plt.show()  # doctest: +SKIP
 
-.. image:: img/example.png
+.. image:: assets/example.png
     :width: 1200
     :align: center
     :alt: (Diversification, ROC)-Graph
